80f4b552d36aa4ae5e238a3d909c9c20e203a3fa
Selcuk Cihan <selcukcihan@gmail.com>
2024-05-23 18:31:16 +0300

    Opengraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,36 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const title = "Dev Quiz"
+const description = "Test your programming knowledge with daily quizzes."
+
 export const metadata: Metadata = {
-  title: "Dev Quiz",
-  description: "Test your programming knowledge with daily quizzes.",
+  title,
+  description,
+  applicationName: title,
+  keywords: ['quiz', 'programming', 'dev', 'developer', 'coding', 'code', 'software', 'engineer', 'web', 'app', 'application', 'site', 'website', 'daily', 'challenge', 'test', 'knowledge', 'skills', 'learning', 'education', 'fun', 'game', 'puzzle', 'problem', 'solution', 'answer', 'question', 'multiple', 'choice', 'true', 'false', 'boolean', 'score', 'leaderboard', 'top', 'best', 'rank'],
+  twitter: {
+    card: 'summary_large_image',
+    title,
+    description,
+    siteId: '134182720',
+    creator: '@scihan',
+    creatorId: '134182720',
+    images: [`https://quiz.selcukcihan.com/icon.png`],
+  },
+  openGraph: {
+    siteName: title,
+    title,
+    type: 'website',
+    url: 'https://quiz.selcukcihan.com',
+    description,
+    images: [{
+      url: `https://quiz.selcukcihan.com/icon.png`,
+      width: 1000,
+      height: 1304,
+      alt: title,
+    }],
+  },
 };
 
 export default function RootLayout({
