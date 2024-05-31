0adb2ef657519fe9cedcb8cc4b7ae382d1cc2983
Selcuk Cihan <selcukcihan@gmail.com>
2024-05-31 17:35:40 +0300

    Add share twitter button

--- components/results-dialog.tsx
+++ components/results-dialog.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react"
 import { QuizSubmission } from "../core/db"
 import { Button } from "./ui/button"
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "./ui/dialog"
+import Link from "next/link"
 
 const getOutcome = (userQuiz: QuizSubmission) => {
   if (!userQuiz) return
@@ -20,6 +21,13 @@ const getOutcome = (userQuiz: QuizSubmission) => {
     default:
       return 'Not so bad, better luck next time!'
   }
+}
+
+const getTweetShareLink = (userQuiz?: QuizSubmission) => {
+  const totalQuestions = userQuiz?.answers.length
+  const score = userQuiz?.score
+  const url = 'https://quiz.selcukcihan.com'
+  return `https://twitter.com/intent/tweet?text=I just scored ${score} out of ${totalQuestions} on this quiz! Try it out at ${url}&hashtags=DevQuiz`
 }
 
 export function ResultsDialog(props: any) {
@@ -47,9 +55,36 @@ export function ResultsDialog(props: any) {
             <h3 className="text-xl font-bold">Your Score</h3>
             <p className="text-4xl font-bold">{userQuiz?.score} / {userQuiz?.answers.length}</p>
           </div>
-          <Button variant="outline" onClick={() => setOpen(false)}>Close</Button>
+          <div className="flex items-center space-x-4">
+            <Link href={getTweetShareLink(userQuiz)} target='_blank'>
+              <Button variant="outline">
+                <TwitterIcon className="h-4 w-4 mr-2" />
+                Share on Twitter
+              </Button>
+            </Link>
+            <Button variant="outline" onClick={() => setOpen(false)}>Close</Button>
+          </div>
         </div>
       </DialogContent>
     </Dialog>
+  )
+}
+
+function TwitterIcon(props: any) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z" />
+    </svg>
   )
 }
