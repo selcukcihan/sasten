e3cbba839787bfb103d7333f09317bb240f2677c
Selcuk Cihan <selcukcihan@gmail.com>
2024-05-31 18:15:27 +0300

    Make user stats a separate component

--- components/questions-view.tsx
+++ components/questions-view.tsx
@@ -25,6 +25,7 @@ import { LeaderBoardUser, Quiz, QuizSubmission, User } from '../core/db'
 import { useState, useEffect, useRef } from "react"
 import { submitQuiz, submitSignIn } from "../app/actions"
 import { useFormStatus } from "react-dom"
+import { UserStats } from "./user-stats"
 
 enum OptionButtonState {
   Unanswered,
@@ -152,22 +153,7 @@ export function QuestionsView(props: any) {
           </form>
         </div>
         }
-        {user?.id && (
-          <div className="dark:text-gray-100 px-8 lg:flex-1 pt-8 place-self-center">
-            <div className="w-36 flex flex-row">
-              <span className="flex-1">Total score</span>
-              <span>{user.score}</span>
-            </div>
-            <div className="w-36 flex flex-row">
-              <span className="flex-1">Games played</span>
-              <span>{user.gamesPlayed}</span>
-            </div>
-            <div className="w-36 flex flex-row">
-              <span className="flex-1">Ranking</span>
-              <span>{getUsersRanking()}/{allScores.length}</span>
-            </div>
-          </div>
-        )}
+        <UserStats user={user} allScores={allScores} />
       </div>
     </main>
   )
---
+++ components/user-stats.tsx
@@ -0,0 +1,31 @@
+import { LeaderBoardUser, User } from "../core/db"
+
+export function UserStats(props: any) {
+  const allScores = props.allScores as LeaderBoardUser[]
+  const user = props.user as User | undefined
+  const getUsersRanking = () => {
+    if (!user) return 0
+    return allScores.sort((a, b) => b.score - a.score).findIndex(u => u.userId === user.id) + 1
+  }
+
+  if (!user) {
+    return null
+  }
+
+  return (
+    <div className="dark:text-gray-100 px-8 lg:flex-1 pt-8 place-self-center">
+      <div className="w-36 flex flex-row">
+        <span className="flex-1">Total score</span>
+        <span>{user.score}</span>
+      </div>
+      <div className="w-36 flex flex-row">
+        <span className="flex-1">Games played</span>
+        <span>{user.gamesPlayed}</span>
+      </div>
+      <div className="w-36 flex flex-row">
+        <span className="flex-1">Ranking</span>
+        <span>{getUsersRanking()}/{allScores.length}</span>
+      </div>
+    </div>
+  )
+}
