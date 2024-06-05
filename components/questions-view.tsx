9d67856df9d5cab036ef867f5f148136d7d228b6
Selcuk Cihan <selcukcihan@gmail.com>
2024-06-05 18:42:11 +0300

    Adjust size for question title

--- components/questions-view.tsx
+++ components/questions-view.tsx
@@ -108,7 +108,7 @@ export function QuestionsView(props: any) {
     <>
       {!loading && <main className={`flex-1 lg:bg-gray-100 dark:bg-gray-800 p-4 lg:p-8 flex flex-col items-center ${submitting ? 'pointer-events-none blur-sm' : ''}`}>
         <div className="lg:bg-white lg:dark:bg-gray-900 lg:shadow-lg lg:rounded-lg w-full max-w-3xl p-4 py-2 lg:p-8 flex-1">
-          <div className="grid grid-cols-3 justify-items-center mb-2 lg:mb-6">
+          <div className="grid grid-cols-3 justify-items-center mb-2 lg:mb-6 text-sm lg:text-base">
             <div className="text-gray-500 dark:text-gray-400">Question {currentQuestion + 1} of {quiz.questions.length}</div>
             <div className="text-gray-500 dark:text-gray-400">{quiz.date}</div>
             <div className="text-gray-500 dark:text-gray-400">{userQuiz ? `Score: ${userQuiz.score}` : ''}</div>
